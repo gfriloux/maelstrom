e8b1ef1e0c75404de685957674546c0f47f89f93
discomfitor <michael.blumenkrantz@gmail.com>
2013-03-24 13:00:15 +0000

    add header proto for rss_updated_get function

--- src/include/azy/Azy.h
+++ src/include/azy/Azy.h
@@ -562,6 +562,7 @@ EAPI Eina_List                *azy_rss_items_steal(Azy_Rss *rss);
 EAPI unsigned int azy_rss_ttl_get(const Azy_Rss *rss);
 EAPI unsigned int azy_rss_skipdays_get(const Azy_Rss *rss);
 EAPI unsigned long long azy_rss_skiphours_get(const Azy_Rss *rss);
+EAPI time_t azy_rss_updated_get(const Azy_Rss *rss);
 EAPI const Eina_List                *azy_rss_authors_get(const Azy_Rss *rss);
 EAPI const Eina_List                *azy_rss_contributors_get(const Azy_Rss *rss);
 EAPI const Eina_List                *azy_rss_categories_get(const Azy_Rss *rss);
