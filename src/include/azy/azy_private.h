457f46eedd013dae121da0a93f07eee62df491ac
Vincent Torri <vincent.torri@gmail.com>
2013-08-26 22:11:30 +0200

    fix alloca declaration

--- src/include/azy/azy_private.h
+++ src/include/azy/azy_private.h
@@ -8,6 +8,31 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+#ifdef STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif !defined alloca
+# ifdef __GNUC__
+#  define alloca __builtin_alloca
+# elif defined _AIX
+#  define alloca __alloca
+# elif defined _MSC_VER
+#  include <malloc.h>
+#  define alloca _alloca
+# elif !defined HAVE_ALLOCA
+#  ifdef  __cplusplus
+extern "C"
+#  endif
+void *alloca (size_t);
+# endif
+#endif
 
 #include <ctype.h>
 #include <inttypes.h>
@@ -16,17 +41,6 @@
 #include <Ecore_Con.h>
 #include <Azy.h>
 #include "match.h"
-
-#ifdef HAVE_ALLOCA_H
-#include <alloca.h>
-#elif defined __GNUC__
-#define alloca __builtin_alloca
-#elif defined _AIX
-#define alloca __alloca
-#else
-#include <stddef.h>
-void *alloca(size_t);
-#endif
 
 #define AZY_SERVER_TYPE                0x0f
 
