d76f1c1d4fbbcbd02eeb2b4b606a698fcdf6230a
Guillaume Friloux <kuri@friloux.me>
2015-10-08 09:53:08 +0200

    Removing re2c as it is better to install it or make a buildep on it.
    Most of the time here, when re2c is not installed, ill trigger an autofoo
    bug making internal building of re2c to fail.
    
    When having re2c installed, i trigger a make dist bug.
    
    So just removing it will solve a lot of things.

--- src/bin/azy.mk
+++ src/bin/azy.mk
@@ -1,51 +1,4 @@
-if NEED_RE2C
-NEED_REC = src/bin/re2cbin
-noinst_PROGRAMS += src/bin/re2cbin
-src_bin_re2cbin_SOURCES = \
-src/bin/re2c/code.cpp \
-src/bin/re2c/dfa.cpp \
-src/bin/re2c/main.cpp \
-src/bin/re2c/actions.cpp \
-src/bin/re2c/substr.cpp \
-src/bin/re2c/translate.cpp \
-src/bin/re2c/mbo_getopt.cpp \
-src/bin/re2c/scanner.cpp \
-src/bin/re2c/basics.h \
-src/bin/re2c/dfa.h \
-src/bin/re2c/globals.h \
-src/bin/re2c/ins.h \
-src/bin/re2c/parser.h \
-src/bin/re2c/re_parser.ypp \
-src/bin/re2c/re.h \
-src/bin/re2c/scanner.h \
-src/bin/re2c/substr.h \
-src/bin/re2c/token.h \
-src/bin/re2c/mbo_getopt.h \
-src/bin/re2c/code.h \
-src/bin/re2c/stream_lc.h \
-src/bin/re2c/code_names.h \
-src/bin/re2c/re_parser.h
-
-src_bin_re2cbin_CPPFLAGS = \
-$(BIN_CPPFLAGS) \
--w \
--I$(top_srcdir)/src/bin/re2c \
--I$(top_builddir)/src/bin/re2c
-
-INTERMEDIATE_S += \
-src/bin/re2c/re_parser.cpp \
-src/bin/re2c/re_parser.h
-
-DISTCLEANFILES += \
-src/bin/re_parser.cpp \
-src/bin/re_parser.h
-
-REC = $(top_builddir)/src/bin/re2cbin
-src/bin/re2c/scanner.cpp: src/bin/re2c/re_parser.h
-else
-NEED_REC =
 REC = $(RE2C)
-endif
 
 DISTCLEANFILES += \
 src/bin/azy_parser.c \
@@ -85,7 +38,7 @@ $(top_builddir)/src/lib/libmaelstrom.la
 src/bin/lemon$(EXEEXT_FOR_BUILD):
 	$(CC_FOR_BUILD) -o $@ -w $(CFLAGS_FOR_BUILD) $(LDFLAGS_FOR_BUILD) src/bin/azy/lemon.c
 
-src/bin/azy_parser.y: src/bin/azy/azy_parser.yre $(NEED_REC) src/bin/lemon$(EXEEXT_FOR_BUILD)
+src/bin/azy_parser.y: src/bin/azy/azy_parser.yre src/bin/lemon$(EXEEXT_FOR_BUILD)
 	$(REC) $(RE2C_OPTS) $<
 
 src/bin/azy_parser.c: src/bin/azy_parser.y src/bin/lemon$(EXEEXT_FOR_BUILD)
