d96ae895f9cc8c7f25a19d5588ef8d032c365ae8
dmoity <damien.moity@amtrust.com>
2015-11-06 14:31:15 +0100

    azy : add missing files for unit tests

--- src/tests/azy/unit/Makefile.mk
+++ src/tests/azy/unit/Makefile.mk
@@ -1,4 +1,5 @@
-check_PROGRAMS += src/tests/azy/unit/t001_content
+check_PROGRAMS += src/tests/azy/unit/t001_content \
+  src/tests/azy/unit/t002_json
 
 src_tests_azy_unit_t001_content_CPPFLAGS = $(AZY_TEST_CPPFLAGS)
 
@@ -8,3 +9,20 @@ src_tests_azy_unit_t001_content_SOURCES = \
 src_tests_azy_unit_t001_content_LDADD = \
   @AZY_LIBS@ \
   $(top_builddir)/src/lib/libmaelstrom.la
+
+BUILT_SOURCES = .sources_t002_json
+src_tests_azy_unit_t002_json_SOURCES = \
+  src/tests/azy/unit/azy_content_deserialize_json.c \
+  src/tests/azy/unit/t002_json.c \
+  src/tests/azy/unit/t002_json.h \
+  src/tests/azy/unit/T002_Common_Azy.c \
+  src/tests/azy/unit/T002_Common_Azy.h \
+  src/tests/azy/unit/T002_Common.c \
+  src/tests/azy/unit/T002_Common.h
+src_tests_azy_unit_t002_json_CPPFLAGS = $(AZY_TEST_CPPFLAGS)
+src_tests_azy_unit_t002_json_LDFLAGS = $(AZY_TEST_LDFLAGS) -lz -lrt -lm -lcheck -lc
+src_tests_azy_unit_t002_json_LDADD = \
+  @AZY_LIBS@ \
+  $(top_builddir)/src/lib/libmaelstrom.la
+.sources_t002_json: src/tests/azy/unit/t002_json.azy src/bin/azy_parser
+	src/bin/azy_parser -H -p -o src/tests/azy/unit/ $(top_srcdir)/src/tests/azy/unit/t002_json.azy
