9c4bb650ab2e557a15eca191f6926f0606323794
Guillaume Friloux <guillaume@friloux.me>
2016-07-15 10:55:49 +0200

    Still trying to fix cross compilation.

--- src/bin/azy.mk
+++ src/bin/azy.mk
@@ -15,9 +15,11 @@ src/bin/y.tab.h
 
 RE2C_OPTS = -bi -o src/bin/azy_parser.y
 
-bin_PROGRAMS += src/bin/azy_parser
+azy_parser = src/bin/azy_parser$(EXEEXT_FOR_BUILD)
 
-src_bin_azy_parser_SOURCES = \
+bin_PROGRAMS += azy_parser
+
+azy_parser_SOURCES = \
 src/bin/azy/azy.h \
 src/bin/azy/azy.c \
 src/bin/azy/azy_parser_main.c \
@@ -25,12 +27,12 @@ src/bin/azy/azy_parser_lib.h \
 src/bin/azy/azy_parser_lib.c \
 src/bin/azy_parser.y
 
-src_bin_azy_parser_CPPFLAGS = \
+azy_parser_CPPFLAGS = \
 $(BIN_CPPFLAGS) \
 $(AZY_CFLAGS) \
 -I$(top_srcdir)/src/bin/azy
 
-src_bin_azy_parser_LDADD = \
+azy_parser_LDADD = \
 $(AZY_LIBS) \
 $(top_builddir)/src/lib/libmaelstrom.la
 
